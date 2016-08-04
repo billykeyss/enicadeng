d6b6b9f91d83c4e7d406c6005595b9cfd15b8a4e
Bill Huang <billhuang@live.ca>
2016-08-04 01:05:12 -0400

    Add all pages

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,39 @@ router.get('/project1', function(req, res, next) {
   res.render('project1', { title: 'Express' });
 });
 
+/* GET project2 page. */
+router.get('/project2', function(req, res, next) {
+  res.render('project2', { title: 'Express' });
+});
+
+/* GET project3 page. */
+router.get('/project3', function(req, res, next) {
+  res.render('project3', { title: 'Express' });
+});
+
+/* GET concept1 page. */
+router.get('/concept1', function(req, res, next) {
+  res.render('concept1', { title: 'Express' });
+});
+
+/* GET project2 page. */
+router.get('/concept2', function(req, res, next) {
+  res.render('concept2', { title: 'Express' });
+});
+
+/* GET concept3 page. */
+router.get('/concept3', function(req, res, next) {
+  res.render('concept3', { title: 'Express' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
 module.exports = router;
